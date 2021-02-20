c89ab482e552f3ae739cf7b1bcd62de295d5c9b0
Hussein Al Hammad <husseinalhammad@gmail.com>
2021-02-20 11:11:32 +0400

    Update app.js

--- pipit_members/js/app.js
+++ pipit_members/js/app.js
@@ -1,20 +1,21 @@
 window.addEventListener('load', (event) => {
     const pageUrl = window.location.pathname.replace(/\/+$/, "");
+    let icon = '';
+    try {
+        icon = Perch.UI.Helpers.icon('ext/o-cloud-download', 14)
+    } catch (e) {
+        // could not use Perch.UI.Helpers.icon()
+    }
 
-
-    console.log(Perch.UI.Helpers)
-    console.log(`${Perch.path}/addons/apps/perch_members`)
-    
     if (pageUrl == `${Perch.path}/addons/apps/perch_members`) {
-        let smartbar = document.querySelector('.smartbar ul');
 
-        smartbar.insertAdjacentHTML('beforeend', `
+        document.querySelector('.smartbar ul').insertAdjacentHTML('beforeend', `
     <li class="smartbar-end smartbar-util">
         <a href="${Perch.path}/addons/apps/pipit_members/export/" title="Download CSV">
-            ${Perch.UI.Helpers.icon('ext/o-cloud-download', 14)}
-            <span>Download CSV</span>
+            ${icon}<span>Download CSV</span>
         </a>
     </li>
     `);
+
     }
 });
